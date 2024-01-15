b41529e68cf7896688dea1fdeb6fb7edb80f0ac6
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-01-15 22:58:37 +0100

    Update simple_example.py

--- pages/simple_example.py
+++ pages/simple_example.py
@@ -5,9 +5,11 @@ import os
 
 load_dotenv()
 
-api_key = os.getenv("OPENAI_API_KEY")
+api_key1 = st.secrets["OPENAI_API_KEY"]
 
-chat_model = ChatOpenAI(openai_api_key=api_key)
+api_key2 = os.getenv("OPENAI_API_KEY")
+
+chat_model = ChatOpenAI(openai_api_key=api_key1)
 
 result = chat_model.predict("Hola dime que es python")
 
