e0a80ac0e37465657df91b5f93f95487e032564a
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-01-16 00:52:34 +0100

    Update chain.py

--- pages/chain.py
+++ pages/chain.py
@@ -1,1 +1,28 @@
+import streamlit as st
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import ChatPromptTemplate
+from langchain.schema import BaseOutputParser
+import os
 
+
+api_key1 = st.secrets["OPENAI_API_KEY"]
+
+chat_model = ChatOpenAI(openai_api_key=api_key1)
+
+class CommaSeparatedListOutputParser(BaseOutputParser):
+  def parse(self, text: str):
+    return text.strip().split(", ")
+
+template = """You area helpful assitant who generates comma separated lists.
+a user will pass in a category, and you should generate 5 objects in that category in a comma separated list.
+ONLY return a commma separated list, and nothing more."""
+human_template = "{text}"
+
+chat_prompt = ChatPromptTemplate.from_messages([
+  ("system", template),
+  ("human", human_template),
+])
+
+chain = chat_prompt | chat model | CommaSeparatedListOutputParser()
+result = chain.invoke({"text": "colors"})
+st.write(result)
