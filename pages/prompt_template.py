e9c262b1cfcb8d62c0beaf199c2afb734b2d8e9f
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-01-16 00:29:47 +0100

    Update prompt_template.py

--- pages/prompt_template.py
+++ pages/prompt_template.py
@@ -1,1 +1,27 @@
+import streamlit as st
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import ChatPromptTemplate
+import os
 
+
+api_key1 = st.secrets["OPENAI_API_KEY"]
+
+api_key2 = os.getenv("OPENAI_API_KEY")
+
+chat_model = ChatOpenAI(openai_api_key=api_key1)
+
+template = "You area helpful assitant that translates {input_language} to {output_language}."
+human_template = "{text}"
+
+chat_prompt = ChatPromptTemplate.from_messages([
+  ("system", template),
+  ("human", human_template),
+])
+
+messages = chat_prompt.format_messages(input_Language="English",
+                                       output_Language="French",
+                                       text="I love programming recommender systems"
+
+result = chat_model.predict_messages(messages)
+
+st.write(result.content)
