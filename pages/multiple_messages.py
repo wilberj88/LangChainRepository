601613c853d0479654f540c41aecb82dff74aaab
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-01-15 23:12:41 +0100

    Update multiple_messages.py

--- pages/multiple_messages.py
+++ pages/multiple_messages.py
@@ -1,1 +1,19 @@
+import streamlit as st
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage
+import os
 
+
+api_key1 = st.secrets["OPENAI_API_KEY"]
+
+api_key2 = os.getenv("OPENAI_API_KEY")
+
+chat_model = ChatOpenAI(openai_api_key=api_key1)
+
+messages = [HumanMessage(content="from now on 1+1=3, use this in your replies"),
+            HumanMessage(content="What is 1+1"),
+            HumanMessage(content="What is 1+1+1?")]           ]
+
+result = chat_model.predict_messages(messages)
+
+st.write(result.content)
