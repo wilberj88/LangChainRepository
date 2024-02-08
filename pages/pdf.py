b36bb0538834eda61578dd0997683aa7e18f5345
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-02-08 17:33:43 +0100

    Update pdf.py

--- pages/pdf.py
+++ pages/pdf.py
@@ -11,6 +11,7 @@ from langchain.vectorstores import FAISS
 from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.callbacks import get_openai_callback
+import os
 
 loader = PyPDFLoader("data/E24AlcaldiaBog2019.pdf")
 pages = loader.load_and_split()
