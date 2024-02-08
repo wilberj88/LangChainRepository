78441e4f8592537324d822e6d7833da217713a7d
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-02-08 17:12:13 +0100

    Update pdf.py

--- pages/pdf.py
+++ pages/pdf.py
@@ -5,4 +5,4 @@ from langchain_community.document_loaders import PyPDFLoader
 loader = PyPDFLoader("data/E24AlcaldiaBog2019.pdf")
 pages = loader.load_and_split()
 
-st.write(pages[0])
+st.write(pages)
